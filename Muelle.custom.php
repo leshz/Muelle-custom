417754dc163177eea7a22acc73721128cc63062e
Jeffer <leshz@me.com>
2017-11-28 16:34:24 +0000

    test

--- Muelle.custom.php
+++ Muelle.custom.php
@@ -1,9 +1,9 @@
 <?php
 /**
-	Plugin Name:		Muelle-wp-custom
+	Plugin Name:	Muelle-wp-custom
 	Plugin URI:		http://github.com/leshz/
-	Description:		Plugin para mostrar estado del muelle e informacion 
-	Version: 		1.0.0
+	Description:	Plugin para mostrar estado del muelle e informacion 
+	Version: 		0.1.0
 	Author:			Jeffer Barragán
 	Author URI:		github.com/leshz
 	License:		GPLv2 or later
@@ -16,11 +16,11 @@
 
 function mapEdit_plugin_menu(){
 	add_menu_page(	'Tiempo Real Muelle',				//Título de la página
-									'Muelle',							//Título del menú
-									'administrator',					//Rol que puede acceder
-									'muelle_form_settings-page',		//Id de la página de opciones
-									'muelle_form_settings',				//Función que pinta la página de configuración del plugin
-									'dashicons-admin-site');			//Icono del menú
+					'Muelle',							//Título del menú
+					'administrator',					//Rol que puede acceder
+				  	'muelle_form_settings-page',		//Id de la página de opciones
+				  	'muelle_form_settings',				//Función que pinta la página de configuración del plugin
+				  	'dashicons-admin-site');			//Icono del menú
 }
 add_action('admin_menu','mapEdit_plugin_menu');
 
@@ -35,21 +35,20 @@ function installDB () {
 	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name){
 		$charset_collate = $wpdb->get_charset_collate();
 		$sql = "CREATE TABLE $table_name (`id` int(3) NOT NULL,
-		  `motonave` varchar(75) COLLATE utf8_spanish_ci NOT NULL,
-		  `muelle_actual` int(2) DEFAULT NULL,
-		  `orientacion` int(2) DEFAULT NULL,
+		  `motonave` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+		  `muelle_actual` int(11) DEFAULT NULL,
+		  `orientacion` int(11) DEFAULT NULL,
 		  `fecha_atrac` varchar(11) COLLATE utf8_spanish_ci DEFAULT NULL,
 		  `hora` varchar(8) COLLATE utf8_spanish_ci DEFAULT NULL,
-		  `agente` varchar(75) COLLATE utf8_spanish_ci DEFAULT NULL,
-		  `client_princp` varchar(99) COLLATE utf8_spanish_ci DEFAULT NULL,
-		  `producto` varchar(99) COLLATE utf8_spanish_ci DEFAULT NULL,
-	      `eslora` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-	      `calado` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL, 
-		  `ton_anun` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-		  `ton_desc` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-		  `ton_acum` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-		  `sal-motonave` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL) $charset_collate;";
-		  
+		  `agente` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+		  `client_princp` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+		  `producto` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+	      `eslora` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+	      `calado` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL, 
+		  `ton_anun` int(20) DEFAULT NULL,
+		  `ton_desc` int(20) DEFAULT NULL ,
+		  `ton_acum` int(20) DEFAULT NULL,
+		  `sal-motonave` int(20) DEFAULT NULL) $charset_collate;";
 		dbDelta( $sql );
 		$sql = "ALTER TABLE $table_name ADD PRIMARY KEY (`id`);";
 		$wpdb->query($sql);
@@ -98,7 +97,7 @@ function muelle_form_settings(){
 						<div class="front">
 							<div class="four columns">
 								<label>Motonave</label>
-								<input class="u-full-width motonave" maxlength="75" name="<?php echo$form; ?>[motonave]" value="<?php echo $item['motonave']; ?>" type="text" required />
+								<input class="u-full-width motonave" name="<?php echo$form; ?>[motonave]" value="<?php echo $item['motonave']; ?>" type="text" required />
 							</div>
 							<div class="two columns">
 								<label>Muelle</label>
@@ -151,19 +150,19 @@ function muelle_form_settings(){
 								</div>
 								<div class="two-haf columns">
 									<label>Agente Maritimo</label>
-									<input class="u-full-width"  maxlength="75" name="<?php echo$form; ?>[agente]" value="<?php echo $item['agente']; ?>"type="text" />
+									<input class="u-full-width" name="<?php echo$form; ?>[agente]" value="<?php echo $item['agente']; ?>"type="text" />
 								</div>
 								<div class="two-haf columns">
 									<label>Clientes Principales</label>
-									<input class="u-full-width" maxlength="99"  name="<?php echo$form; ?>[cliente]" type="text"  value="<?php echo $item['client_princp']; ?>"/>
+									<input class="u-full-width" name="<?php echo$form; ?>[cliente]" type="text"  value="<?php echo $item['client_princp']; ?>"/>
 								</div>
 								<div class="two-haf columns">
 									<label>Producto</label>
-									<input class="u-full-width" maxlength="99" name="<?php echo$form; ?>[producto]" type="text" value="<?php echo $item['producto']; ?>" />
+									<input class="u-full-width" name="<?php echo$form; ?>[producto]" type="text" value="<?php echo $item['producto']; ?>" />
 								</div>
 								<div class="two-haf columns">
 									<label>Eslora</label>
-									<input class="u-full-width" id="slora" maxlength="8" name="<?php echo$form; ?>[eslora]" type="text" value="<?php echo $item['eslora']; ?>" />
+									<input class="u-full-width" name="<?php echo$form; ?>[eslora]" type="text" value="<?php echo $item['eslora']; ?>" />
 								</div>
 							
 							
@@ -171,23 +170,23 @@ function muelle_form_settings(){
 							<div class="row">
 								<div class="two-haf columns">
 									<label>Calado</label>
-									<input class="u-full-width" id="calado"  maxlength="8"  name="<?php echo $form; ?>[calado]"type="text"  value="<?php echo $item['calado']; ?>"  />
+									<input class="u-full-width" name="<?php echo $form; ?>[calado]"type="text"  value="<?php echo $item['calado']; ?>"  />
 								</div>
 								<div class="two-haf columns">
 									<label>Tonelaje Anunciado</label>
-									<input class="u-full-width" id="ton"  maxlength="20" name="<?php echo $form; ?>[tonelaje-anun]" type="text" value="<?php echo $item['ton_anun']; ?>" />
+									<input class="u-full-width" id="ton" name="<?php echo $form; ?>[tonelaje-anun]" type="text" value="<?php echo $item['ton_anun']; ?>" />
 								</div>
 								<div class="two-haf columns">
 									<label>Tonelaje Descargado</label>
-									<input class="u-full-width"   maxlength="20" id="ton"name="<?php echo $form; ?>[tonelaje-desc]" type="text" value="<?php echo $item['ton_desc']; ?>" />
+									<input class="u-full-width"  id="ton"name="<?php echo $form; ?>[tonelaje-desc]" type="text" value="<?php echo $item['ton_desc']; ?>" />
 								</div>
 								<div class="two-haf columns">
 									<label>Tonelaje Acumulado</label>
-								<input class="u-full-width" id="ton" maxlength="20" name="<?php echo $form; ?>[tonelaje-acum]" type="text" value="<?php echo $item['ton_acum']; ?>" />
+								<input class="u-full-width" id="ton" name="<?php echo $form; ?>[tonelaje-acum]" type="text" value="<?php echo $item['ton_acum']; ?>" />
 								</div>
 								<div class="two-haf columns">
 									<label>Saldo Motonave</label>
-									<input class="u-full-width" id="ton" maxlength="20" name="<?php echo $form; ?>[sal-motonave]"type="text" value="<?php echo $item['sal-motonave']; ?>" />
+									<input class="u-full-width" name="<?php echo $form; ?>[sal-motonave]"type="text" value="<?php echo $item['sal-motonave']; ?>" />
 								</div>
 							</div>
 						</div>
@@ -254,9 +253,8 @@ add_action('wp_ajax_deleteField', 'deleteField');
 
 
 function limpiarString($texto) {
-    //$textoLimpio = preg_replace('([^0-9])', '', $texto);
-    //return $textoLimpio;
-    return $texto;
+    $textoLimpio = preg_replace('([^0-9])', '', $texto);
+    return $textoLimpio;
 }
 
 
@@ -264,8 +262,6 @@ function limpiarString($texto) {
 add_action( 'admin_post_process_form', 'process_form_data' );
 
 function process_form_data() {
-//	$wpdb->show_errors = true;
-	//$wpdb->suppress_errors = false;
 	global $wpdb;
 	$table_name = $wpdb->prefix . 'muelle_status';	 
 	$formInfo = $_POST;
@@ -310,9 +306,7 @@ function process_form_data() {
 										));
 		}	
 }	
-
-wp_redirect( $_SERVER['HTTP_REFERER'] );
-//return $wpdb->print_error();
+	wp_redirect( $_SERVER['HTTP_REFERER'] );
 }
 
 
@@ -340,7 +334,7 @@ function muelle_status() {
 			</div>	
 		</div>
 		<div class="muelle_back">
-			<img src="<?php echo plugins_url( 'assets/client/img/mapa-puerto-2.png', __FILE__ ) ?>" draggable="false" />
+			<img src="<?php echo plugins_url( 'assets/client/img/mapa-puerto.png', __FILE__ ) ?>" draggable="false" />
 			<div class="battleship">
 			<?php
 			if( $datainfo[0]['motonave'] != "" ){
@@ -419,19 +413,19 @@ function muelle_status() {
 	                </tr>
 	                <tr>
 	                    <th>Tonelaje Anunciado</th>
-	                    <td><?php echo $ship['ton_anun']; ?> TM</td>
+	                    <td><?php echo $ship['ton_anun']; ?></td>
 	                </tr>
 	                <tr>
 	                    <th>Tonelaje Descargado</th>
-	                    <td><?php echo $ship['ton_desc']; ?> TM</td>
+	                    <td><?php echo $ship['ton_desc']; ?></td>
 	                </tr>
 	                 <tr>
 	                    <th>Tonelaje Acumulado</th>
-	                    <td><?php echo $ship['ton_acum']; ?> TM</td>
+	                    <td><?php echo $ship['ton_acum']; ?></td>
 	                </tr>
 	                 <tr>
 	                    <th>Saldo motonave</th>
-	                    <td><?php echo $ship['sal-motonave']; ?> TM</td>
+	                    <td><?php echo $ship['sal-motonave']; ?></td>
 	                </tr>
 	                <tr>
 	                    <th class="ulti">Atracado en Muelle #</th>
@@ -478,10 +472,11 @@ function getFiveLastpdf($atts){
 				$hora = get_the_date('j \d\e\ F \d\e\ Y', $attachment);
 				echo "<option value='{$attachment->guid}'>{$hora}</option>";
 			}
-	}
+		}
 ?>
 
 	</select>
+
 	
 	<button>Descargar Situacion portuaria</button>
 	
